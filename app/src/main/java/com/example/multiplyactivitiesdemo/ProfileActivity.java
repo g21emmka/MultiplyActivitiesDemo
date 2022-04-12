5ff749982e7986230bbd1f332ee20700780365fa
g21emmka <g21emmka@student.his.se>
2022-04-12 15:20:05 +0200

    Added parentActivityName in manifest

--- app/src/main/java/com/example/multiplyactivitiesdemo/ProfileActivity.java
+++ app/src/main/java/com/example/multiplyactivitiesdemo/ProfileActivity.java
@@ -3,12 +3,45 @@ package com.example.multiplyactivitiesdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("==>", "ProfileAcitivty started.");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("==>", "ProfileAcitivty stopped.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("==>", "MProfileAcitivty destroyed.");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("==>", "ProfileAcitivty paused.");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("==>", "ProfileAcitivty resumed.");
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+            Log.d("==>", "ProfileAcitivty created.");
     }
 }
