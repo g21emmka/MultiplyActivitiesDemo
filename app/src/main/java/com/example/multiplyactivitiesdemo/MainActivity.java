dd89585ca39b96a97ce5a8336a4cea7b27d46ad3
g21emmka <g21emmka@student.his.se>
2022-04-12 15:00:05 +0200

    Added intent

--- app/src/main/java/com/example/multiplyactivitiesdemo/MainActivity.java
+++ app/src/main/java/com/example/multiplyactivitiesdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.multiplyactivitiesdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -53,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d("==>", "Sign in button pressed.");
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
 
             }
         });
