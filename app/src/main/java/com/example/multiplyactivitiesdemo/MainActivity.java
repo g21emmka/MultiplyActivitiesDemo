8e522ed9430562ea083c687513734a0163c95442
g21emmka <g21emmka@student.his.se>
2022-04-12 13:57:48 +0200

    created onStart, onStop, onDestroy, onPause, onResume

--- app/src/main/java/com/example/multiplyactivitiesdemo/MainActivity.java
+++ app/src/main/java/com/example/multiplyactivitiesdemo/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.multiplyactivitiesdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("==>", "MainAcitivty started.");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("==>", "MainAcitivty stopped.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("==>", "MainAcitivty destroyed.");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("==>", "MainAcitivty paused.");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("==>", "MainAcitivty resumed.");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("==>", "MainAcitivty created.");
     }
 }
